648480f4cf0ecca03620b3f902c14bce6db0608a
Andrew Sidorenko <sidorenkobw@gmail.com>
2017-03-03 21:35:25 -0800

    Improved learning app view

--- app/learn/view/learn.php
+++ app/learn/view/learn.php
@@ -15,20 +15,20 @@ $this->getLayout()->body_scripts .= '<script src="/js/learn.js?build=' . $this->
 
 <div class="controlsPanel">
     <div class="row">
-        <div class="col-md-3">
-            <button id="btnLearnRetry" class="btn-learn-retry btn btn-danger btn-lg">Retry Current Word <span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button>
+        <div class="col-md-4 col-sm-4">
+            <button id="btnLearnRetry" class="btn-learn-retry btn btn-danger btn-lg">Retry Word <span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button>
         </div>
-        <div class="col-md-6" style="text-align: center">
+        <div class="col-md-4 col-sm-4" style="text-align: center">
             <button id="btnLearnToggleRecording" class="btn btn-primary btn-lg">Start Recording <span class="glyphicon glyphicon-ice-lolly" aria-hidden="true"></span></button>
         </div>
-        <div class="col-md-3">
-            <button id="btnLearnNext" class="btn btn-success btn-lg" style="margin-left: 10px">Next Word <span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span></button>
+        <div class="col-md-4 col-sm-4">
+            <button id="btnLearnNext" class="btn btn-success btn-lg pull-right" style="margin-left: 10px">Next Word <span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span></button>
             
         </div>
     </div>
     
     <div class="row">
-        <div class="col-md-6 col-md-offset-3" style="text-align: center">
+        <div class="col-md-12" style="text-align: center">
             <button id="btnLearnToggleEdit" class="btn btn-lg"><span>Edit Text</span> <span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button>
         </div>
     </div>
