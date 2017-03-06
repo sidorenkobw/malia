1e718c2c649bdec7e9c7a59048d12a4babddbe73
Andrew Sidorenko <sidorenkobw@gmail.com>
2017-03-05 18:03:29 -0800

    Improved buttons

--- app/learn/view/learn.php
+++ app/learn/view/learn.php
@@ -19,26 +19,8 @@ $this->getLayout()->body_scripts .= '<script src="/js/learn.js?build=' . $this->
 
     <div class="controlsPanel">
         <div class="row">
-            <div class="col-md-4" style="text-align: center">
-                <button id="btnFullScreen" class="btn btn-default btn-lg" title="Switch to read mode. Keys: Alt + Enter to enter or exit fullscreen mode">
-                    <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span> <span class="caption">Full Screen</span>
-                </button>
-            </div>
-            <div class="col-md-4" style="text-align: center">
-                <button id="btnLearnToggleEdit" class="btn btn-default btn-lg" title="Modify current text. Keys: Esc - to exit edit mode">
-                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> <span class="caption">Edit Text</span>
-                </button>
-            </div>
-            <div class="col-md-4" style="text-align: center">
-                <button id="btnSkipWord" class="btn btn-default btn-lg" disabled="disabled" title="Modify current text. Keys: Esc - to exit edit mode">
-                    <span class="caption">Skip Word</span> <span class="glyphicon glyphicon-fast-forward" aria-hidden="true"></span>
-                </button>
-            </div>
-        </div>
-        
-        <div class="row">
             <div class="col-md-4 col-sm-4">
-                <button id="btnLearnRetry" class="btn-learn-retry btn btn-danger btn-lg" disabled="disabled" title="Cancel current word record and try again. Keys: Backspace">
+                <button id="btnLearnRetry" class="btn-learn-retry btn btn-danger btn-lg" tabindex="3" disabled="disabled" title="Cancel current word record and try again. Keys: Backspace">
                     <span class="glyphicon glyphicon-repeat" aria-hidden="true"></span> Retry Word
                 </button>
             </div>
@@ -48,8 +30,26 @@ $this->getLayout()->body_scripts .= '<script src="/js/learn.js?build=' . $this->
                 </button>
             </div>
             <div class="col-md-4 col-sm-4">
-                <button id="btnLearnNext" class="btn btn-success btn-lg pull-right" disabled="disabled" title="Save current word and record the next one. Keys: Space or Enter">
+                <button id="btnLearnNext" class="btn btn-success btn-lg pull-right" tabindex="2" disabled="disabled" title="Save current word and record the next one. Keys: Space or Enter">
                     Next Word <span class="glyphicon glyphicon-step-forward" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4" style="text-align: center">
+                <button id="btnFullScreen" class="btn btn-default btn-lg" tabindex="4" title="Switch to read mode. Keys: Alt + Enter to enter or exit fullscreen mode">
+                    <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span> <span class="caption">Full Screen</span>
+                </button>
+            </div>
+            <div class="col-md-4" style="text-align: center">
+                <button id="btnLearnToggleEdit" class="btn btn-default btn-lg" tabindex="4" title="Modify current text. Keys: Esc - to exit edit mode">
+                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> <span class="caption">Edit Text</span>
+                </button>
+            </div>
+            <div class="col-md-4" style="text-align: center">
+                <button id="btnSkipWord" class="btn btn-default btn-lg" tabindex="4" disabled="disabled" title="Modify current text. Keys: Esc - to exit edit mode">
+                    <span class="caption">Skip Word</span> <span class="glyphicon glyphicon-fast-forward" aria-hidden="true"></span>
                 </button>
             </div>
         </div>
