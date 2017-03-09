8e3f8020ef2fd35a67b32cb8b8815d03566d14b3
Drew Perttula <drewp@bigasterisk.com>
2017-03-09 00:59:18 -0800

    learn/server can stream log lines to its console page; train.py actually trains

--- learn/server.py
+++ learn/server.py
@@ -1,9 +1,9 @@
 """
 Web server for driving training and testing remotely.
 """
-import sys
-import cyclone.web
-from twisted.internet import reactor, task
+import sys, time
+import cyclone.web, cyclone.sse
+from twisted.internet import reactor, defer, task
 from twisted.python import log
 from twisted.python.filepath import FilePath
 
@@ -12,6 +12,50 @@ class SoundListing(cyclone.web.RequestHandler):
         top = FilePath('sounds')
         self.write({'sounds': [{'path': '/'.join(p.segmentsFrom(top))} for p in sorted(top.walk()) if p.isfile()]})
 
+class Train(cyclone.web.RequestHandler):
+    def put(self):
+        d = defer.Deferred()
+        self.write("one\n")
+        def two():
+            self.write("two\n")
+            d.callback(None)
+        reactor.callLater(1, two)
+        return d
+
+
+_logWatchers = {}
+        
+class TrainLogs(cyclone.sse.SSEHandler):
+    def bind(self):
+        self.key = time.time()
+        _logWatchers[self.key] = self
+        for line in self.settings.trainRunner.recentLogs:
+            self.sendEvent(event='line', message=line)
+        
+    def unbind(self):
+        del _logWatchers[self.key]
+
+        
+class TrainRunner(object):
+    def __init__(self):
+        self.recentLogs = []
+        task.LoopingCall(self.fake).start(1)
+
+    def restart(self):
+        raise
+
+    def cancel(self):
+        raise
+       
+        
+    def fake(self):
+        line = 'line %s' % time.time()
+        self.recentLogs = self.recentLogs[-100:] + [line]
+        for lw in _logWatchers.values():
+            lw.sendEvent(event='line', message=line)
+
+
+trainRunner = TrainRunner()
 log.startLogging(sys.stderr)
 reactor.listenTCP(
     9990,
@@ -20,5 +64,7 @@ reactor.listenTCP(
         (r'/lib/(.*)', cyclone.web.StaticFileHandler, {"path": "public_html/lib"}),
         (r'/sounds', SoundListing),
         (r'/sounds/(.*\.webm)', cyclone.web.StaticFileHandler, {"path": "sounds"}),
-    ]))
+        (r'/train', Train),
+        (r'/train/logs', TrainLogs),
+    ], trainRunner=trainRunner))
 reactor.run()
--- learn/train.py
+++ learn/train.py
@@ -5,9 +5,11 @@ Read audio files, output model weights file.
 also see
 https://github.com/coopie/speech_ml/blob/master/speech_ml/waveform_tools.py numpy code for random padding
 """
+import numpy
 
 from speechmodel import makeModel
 from loader import load
+import keras.callbacks
 
 def train(out_weights='weights.h5'):
     model = makeModel()
@@ -15,12 +17,19 @@ def train(out_weights='weights.h5'):
     model.compile(loss='mean_squared_error',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
+    for lyr in model.layers:
+        print lyr.input_shape, lyr.output_shape
 
-    x = [load('sounds/incoming/sound-1488957441246.webm'),
-         load('sounds/incoming/sound-1488957653394.webm'),
-         load('sounds/incoming/sound-1488959248054.webm')]
-    y = ['word1', 'word2', 'word1']
-    model.fit(x, y,  batch_size=32, nb_epoch=10, validation_split=.34)
+    x = numpy.array([load('sounds/incoming/sound-1488957441246.webm')[:100],
+                     load('sounds/incoming/sound-1488957653394.webm')[:100],
+                     load('sounds/incoming/sound-1488959248054.webm')[:100]])
+    y = numpy.array([[1,0,0,0,0,0,0,0,0,0],
+                     [0,1,0,0,0,0,0,0,0,0],
+                     [1,0,0,0,0,0,0,0,0,0]])
+
+    tb = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1, write_graph=True)
+
+    model.fit(x, y,  batch_size=32, nb_epoch=10, validation_split=.34, callbacks=[tb])
     model.save_weights(out_weights)
 
 
