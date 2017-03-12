3e6639dece27e9b52aa022101ee5dd11cf5f15af
Drew Perttula <drewp@bigasterisk.com>
2017-03-11 16:35:15 -0800

    learn/ web console progress

--- learn/server.py
+++ learn/server.py
@@ -7,28 +7,28 @@ from twisted.internet import reactor, defer, task
 from twisted.python import log
 from twisted.python.filepath import FilePath
 
+import train
+
 class SoundListing(cyclone.web.RequestHandler):
     def get(self):
         top = FilePath('sounds')
         self.write({'sounds': [{'path': '/'.join(p.segmentsFrom(top))} for p in sorted(top.walk()) if p.isfile()]})
 
-class Train(cyclone.web.RequestHandler):
+class TrainRestart(cyclone.web.RequestHandler):
     def put(self):
-        d = defer.Deferred()
-        self.write("one\n")
-        def two():
-            self.write("two\n")
-            d.callback(None)
-        reactor.callLater(1, two)
-        return d
+        self.settings.trainRunner.restart()
+        self.finish()
 
-
-_logWatchers = {}
+_logWatchers = {} # values are SSEHandler objects with sendEvent method
         
 class TrainLogs(cyclone.sse.SSEHandler):
     def bind(self):
         self.key = time.time()
         _logWatchers[self.key] = self
+        self.resync()
+
+    def resync(self):
+        self.sendEvent(event='clear', message='')
         for line in self.settings.trainRunner.recentLogs:
             self.sendEvent(event='line', message=line)
         
@@ -41,16 +41,19 @@ class TrainRunner(object):
         self.recentLogs = []
         task.LoopingCall(self.fake).start(1)
 
+
     def restart(self):
-        raise
+        train.train(self.onStep)
 
     def cancel(self):
         raise
-       
+
+    def onStep(self, lg):
+        print 'step', lg
         
     def fake(self):
         line = 'line %s' % time.time()
-        self.recentLogs = self.recentLogs[-100:] + [line]
+        self.recentLogs = self.recentLogs[-10:] + [line]
         for lw in _logWatchers.values():
             lw.sendEvent(event='line', message=line)
 
@@ -60,11 +63,12 @@ log.startLogging(sys.stderr)
 reactor.listenTCP(
     9990,
     cyclone.web.Application([
-        (r'/()', cyclone.web.StaticFileHandler, {"path": "learn", "default_filename": "index.html"}),
+        (r'/()', cyclone.web.StaticFileHandler,
+         {"path": "learn", "default_filename": "index.html"}),
         (r'/lib/(.*)', cyclone.web.StaticFileHandler, {"path": "public_html/lib"}),
         (r'/sounds', SoundListing),
         (r'/sounds/(.*\.webm)', cyclone.web.StaticFileHandler, {"path": "sounds"}),
-        (r'/train', Train),
+        (r'/train/restart', TrainRestart),
         (r'/train/logs', TrainLogs),
     ], trainRunner=trainRunner))
 reactor.run()
