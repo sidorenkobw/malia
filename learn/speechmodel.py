2cc99966d98182444a1ab3df518cdc43be0e91a5
Drew Perttula <drewp@bigasterisk.com>
2017-03-14 01:12:25 -0700

    train on a few words with repeats. Sometimes this makes weights with acc=0.25

--- learn/speechmodel.py
+++ learn/speechmodel.py
@@ -3,9 +3,10 @@ from keras.layers import Convolution2D, MaxPooling2D, Activation, Dropout, Flatt
 
 def makeModel():
     model = Sequential()
-    model.add(Dense(output_dim=64, input_dim=20000))
-    model.add(Activation('relu'))
-    model.add(Dense(output_dim=10))
+    model.add(Dense(output_dim=64, input_dim=10000))
+    for d in [64,64,32,32,30]:
+        model.add(Activation('relu'))
+        model.add(Dense(output_dim=d))
     model.add(Activation('softmax'))
 
     return model
--- learn/train.py
+++ learn/train.py
@@ -9,19 +9,23 @@ import speechmodel
 from loader import load
 import keras.callbacks
 
-import audiotransform 
+import audiotransform
+
+def _wordFromPath(p):
+    user, word, timestamp = p.path.split('/')[-3:]
+    return word
 
 def train(callback=None, out_weights='weights.h5'):
     reload(audiotransform)
     reload(speechmodel)
 
-
     words = 'angeles  be  going  los  malia  on  recording  start  to  will add  asdf  click  df  edit  sadf  sdf  text  to  your click  edit test'.split()
+    words = 'click text edit'.split()
+
     repeat = 20
-    goalSize = 10000
+    goalSize = 10000 # samples after padding
     embedSize = 30
 
-    
     model = speechmodel.makeModel()
 
     model.compile(loss='mean_squared_error',
@@ -32,10 +36,13 @@ def train(callback=None, out_weights='weights.h5'):
     paths = []
     for p in sorted(top.walk()):
         if p.isfile():
+            word = _wordFromPath(p)
+            if word not in words:
+                continue
             try:
                 raw = load(p)
                 crop = audiotransform.autoCrop(raw, rate=8000)
-                padDoesntFail = audiotransform.randomPad(crop, goalSize)
+                audiotransform.randomPad(crop, goalSize) # must not error
                 print 'using %s cropped to %s samples' % (p, len(crop))
             except audiotransform.TooQuiet:
                 print '%s too quiet' % p
@@ -46,7 +53,7 @@ def train(callback=None, out_weights='weights.h5'):
     y = numpy.zeros((len(paths) * repeat, embedSize), dtype=numpy.float)
 
     def embed(p, embedSize):
-        user, word, timestamp = p.path.split('/')[-3:]
+        word = _wordFromPath(p)
         vec = [0] * embedSize
         vec[words.index(word)] = 1
         return vec
@@ -63,7 +70,7 @@ def train(callback=None, out_weights='weights.h5'):
     if callback:
         callbacks.append(callback)
 
-    model.fit(x, y, batch_size=500, nb_epoch=20, validation_split=.03,
+    model.fit(x, y, batch_size=500, nb_epoch=20, validation_split=.02,
               callbacks=callbacks)
 
     model.save_weights(out_weights)
