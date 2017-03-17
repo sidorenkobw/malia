c8f1413a3d17376a89ab421e007f19e709489e6d
Drew Perttula <drewp@bigasterisk.com>
2017-03-17 16:50:24 -0700

    soundsdir don't break on unusual paths

--- learn/soundsdir.py
+++ learn/soundsdir.py
@@ -20,12 +20,12 @@ def soundFields(p):
             d['user'] = userForId.get(segments[0], segments[0])
         if len(segments) > 1:
             d['word'] = urllib.unquote(segments[1])
-        if segments[1].endswith('.webm'):
-            # old format
-            d['word'], m = segments[1].split('_', 1)
-            d['milli'] = int(m.split('.')[0])
-        elif len(segments) > 2:
-            d['milli'] = int(segments[2].replace('.webm', ''))
+            if segments[1].endswith('.webm'):
+                # old format
+                d['word'], m = segments[1].split('_', 1)
+                d['milli'] = int(m.split('.')[0])
+            elif len(segments) > 2:
+                d['milli'] = int(segments[2].replace('.webm', ''))
         if 'milli' in d:
             d['iso'] = datetime.datetime.fromtimestamp(d['milli'] / 1000).isoformat('T').split('.')[0]
         return d
