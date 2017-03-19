00d4d7f57839d4361ca2d37247ec1067c79a3749
Drew Perttula <drewp@bigasterisk.com>
2017-03-19 14:31:14 -0700

    new meter and adaptive thresholding

--- public_html/js/view/meter/speak.js
+++ public_html/js/view/meter/speak.js
@@ -4,66 +4,135 @@ define(["view/meter"], function (MeterView) {
             super(elem);
 
             this.canvas = document.createElement("canvas");
-            this.canvas.width = 900;
-            this.canvas.height = 300;
+            this.canvas.width = 700;
+            this.canvas.height = 200;
             this.canvas.style.display = "block";
             this.canvas.style.margin = "0 auto";
             $(elem).append(this.canvas);
             this.ctx = this.canvas.getContext("2d");
 
+            this.oldIsActivated = false;
             this.isActivated = false;
-            this.threshold = 15;
-            this.thresholdCount = 200;
+            this.thresholdSec = .2;
 
             this.minCounter = 0;
+
+            this.prevAvg = 0;
+            this.periodMs = 20;
+            this.historyMs = 5000;
+            this.history = [];
+            this.histAboveThreshold = [];
+            this.thresholdQuiet = 0;
+            this.historicalPeak = 1;
+        }
+
+        start() {
+            super.start();
+            this.redraw();
+        }
+
+        redraw() {
+            let w = this.canvas.width, h = this.canvas.height;
+            this.ctx.clearRect(0, 0, w, h);
+
+            let yFromMag = function(mag) {
+                return (h - 5) * (1 - (mag / this.historicalPeak));
+            }.bind(this);
+
+            this.ctx.strokeStyle = '#ffaa00';
+            this.ctx.beginPath();
+            this.ctx.moveTo(0, yFromMag(this.thresholdQuiet));
+            this.ctx.lineTo(w, yFromMag(this.thresholdQuiet));
+            this.ctx.stroke();
+
+            this.ctx.strokeStyle = '#000000';
+            var x = 0;
+            this.ctx.beginPath();
+            this.ctx.moveTo(x, yFromMag(0));
+            var dx = w / (this.historyMs / this.periodMs);
+            this.history.forEach((mag) => {
+                x = x + dx;
+                this.ctx.lineTo(x, yFromMag(mag));
+            });
+            this.ctx.stroke();
+
+            this.ctx.strokeStyle = '#aa0000';
+            x = 0;
+            this.histAboveThreshold.forEach((above) => {
+                if (above) {
+                    this.ctx.beginPath();
+                    this.ctx.moveTo(x, yFromMag(0));
+                    this.ctx.lineTo(x, yFromMag(1));
+                    this.ctx.stroke();
+                }
+                x = x + dx;
+            });
+
+            requestAnimationFrame(function() {
+                setTimeout(this.redraw.bind(this), this.periodMs);
+            }.bind(this));
         }
 
         update() {
             this.analyser.getByteTimeDomainData(this.dataArray);
-            this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
-
-            var x = 0;
-            this.ctx.beginPath();
-            this.ctx.moveTo(x, Math.round(this.canvas.height/2));
-
-            var maxLevel = 0;
-
+            let avgLevel = 0;
             this.dataArray.forEach((e) => {
-                var mag = Math.abs(e - 128),
-                    oldIsActivated = this.isActivated;
-
-                if (mag > this.threshold) {
-                    this.isActivated = true;
-                    this.minCounter = 0;
-                } else {
-                    this.minCounter++;
-                }
-
-                if (this.minCounter > this.thresholdCount) {
-                    this.isActivated = false;
-                }
-
-                if (oldIsActivated && !this.isActivated) {
-                    this.emit("word-stop");
-                }
-
-                if (!oldIsActivated && this.isActivated) {
-                    this.emit("word-start");
-                }
-
-                x = x + 7;
-                this.ctx.lineTo(x, this.canvas.height/2 - mag);
-
-                if (mag > maxLevel) {
-                    maxLevel = mag;
-                }
+                let mag = Math.abs(e - 128) / 128;
+                avgLevel += mag;
             });
+            avgLevel /= this.dataArray.length;
 
-            this.ctx.stroke();
+            let smooth = .7;
+            let smoothedAvg = smooth * this.prevAvg + (1 - smooth) * avgLevel;
+            this.prevAvg = smoothedAvg;
 
-            requestAnimationFrame(function() {
-                setTimeout(this.update.bind(this), 100);
-            }.bind(this));
+
+            this.minPeak = .3;
+            this.historicalPeak = Math.max(this.minPeak,
+                                           Math.max.apply(null, this.history));
+
+            let histLen = this.historyMs / this.periodMs;
+
+            let v = this.history.slice();
+            v.sort();
+            let smoothQuiet = .8;
+            let currentQuiet = v[Math.floor(histLen * .8)] * .5;
+            if (!isNaN(currentQuiet)) {
+                this.thresholdQuiet = (smoothQuiet * this.thresholdQuiet +
+                                       (1 - smoothQuiet) * currentQuiet);
+            }
+
+            if (smoothedAvg > this.thresholdQuiet) {
+                this.isActivated = true;
+                this.minCounter = 0;
+            } else {
+                this.minCounter++;
+            }
+
+
+            if (this.minCounter > (this.thresholdSec * 1000) / this.periodMs) {
+                this.isActivated = false;
+            }
+
+            if (this.oldIsActivated && !this.isActivated) {
+                this.emit("word-stop");
+            }
+
+            if (!this.oldIsActivated && this.isActivated) {
+                this.emit("word-start");
+            }
+            this.oldIsActivated = this.isActivated;
+
+
+            this.history.push(smoothedAvg);
+            this.histAboveThreshold.push(smoothedAvg > this.thresholdQuiet);
+
+            if (this.history.length > histLen) {
+                this.history.splice(0, 1);
+                this.histAboveThreshold.splice(0, 1);
+            }
+
+            setTimeout(this.update.bind(this), this.periodMs);
         }
     }
 
