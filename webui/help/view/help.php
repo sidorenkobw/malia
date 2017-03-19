d4cd24abd7606b968e0d3a5073b68338a319755d
jtoch <joshuatoch@gmail.com>
2017-03-19 14:34:12 -0700

    Update help.php

--- webui/help/view/help.php
+++ webui/help/view/help.php
@@ -1,22 +1,22 @@
 <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2">
         <h1>Help</h1>
-        <p><bold>Step 1:</bold> In your Chrome Browser go to https://maliaspeech.org/. </p>
-        <p><bold>Step 2:</bold> Log in to the website with your google account.</p>
-        <p><bold>Step 3:</bold> Go to the learn page.</p>
-        <p><bold>Step 4:</bold> Edit the text. You can either type into the consul what you want to read into the program or you can copy and paste a passage that you want to read into the consul.</p>
-        <p><bold>Step 5:</bold> If you are ready to begin recording immediately then you can click “Start Recording.”  If you want the website to save your edits in order to record later then click “Finish Editing.”
+        <p><strong>Step 1:</strong> In your Chrome Browser go to https://maliaspeech.org/. </p>
+        <p><strong>Step 2:</strong> Log in to the website with your google account.</p>
+        <p><strong>Step 3:</strong> Go to the learn page.</p>
+        <p><strong>Step 4:</strong> Edit the text. You can either type into the consul what you want to read into the program or you can copy and paste a passage that you want to read into the consul.</p>
+        <p><strong>Step 5:</strong> If you are ready to begin recording immediately then you can click “Start Recording.”  If you want the website to save your edits in order to record later then click “Finish Editing.”
 </p>
-        <p><bold>Step 6:</bold> Speak into a microphone that it connected to the computer that you are using and make sure you see cat heads after “Incoming sound meter:” (Anywhere from 0 to 5, but they should constantly be fluctuating.)</p>
-        <p><bold>Step 7:</bold> You are ready to start training your personal speech recognizer!!! Click “Start Recording.”</p>
+        <p><strong>Step 6:</strong> Speak into a microphone that it connected to the computer that you are using and make sure you see cat heads after “Incoming sound meter:” (Anywhere from 0 to 5, but they should constantly be fluctuating.)</p>
+        <p><strong>Step 7:</strong> You are ready to start training your personal speech recognizer!!! Click “Start Recording.”</p>
 
-        <p><bold>Step 8:</bold> Say only the word that is boxed in red and then click “Next Word.”</p>
-        <p><bold>Step 9:</bold> Once the next word has the red box around it, you may say it without clicking anything else and it will record the word. Please take note of the fact that the “Words uploaded:” count has in fact changed. 
+        <p><strong>Step 8:</strong> Say only the word that is boxed in red and then click “Next Word.”</p>
+        <p><strong>Step 9:</strong> Once the next word has the red box around it, you may say it without clicking anything else and it will record the word. Please take note of the fact that the “Words uploaded:” count has in fact changed. 
 </p>
-        <p><bold>Step 10:</bold> If you want to skip a word and not record it, the “Next Word” key will move the red box to the next word without recording the previous word. Please take note of the fact that the “Words uploaded:” count has in fact not changed even though you have passed another word. </p>
-        <p><bold>Step 11:</bold> When you get to the last word of the passage or you feel like finishing your recording session click “Stop Recording. 
+        <p><strong>Step 10:</strong> If you want to skip a word and not record it, the “Next Word” key will move the red box to the next word without recording the previous word. Please take note of the fact that the “Words uploaded:” count has in fact not changed even though you have passed another word. </p>
+        <p><strong>Step 11:</strong> When you get to the last word of the passage or you feel like finishing your recording session click “Stop Recording. 
 </p>
-        <p><bold>Troubleshooting:</bold>
+        <p><strong>Troubleshooting:</strong>
 </p>
         <p>If the cats are not appearing on the screen, fully shut down the Chrome internet browser and restart it.</p>
         <p>If the recording times out on you while you are using it, please note that between every word you are given 10 seconds to say the word otherwise you will be timed out. If this is a continuous issue you are running into please get into contact with the builders of the website to get this changed. 
